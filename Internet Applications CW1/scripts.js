7995c8896a06620be935c659dedca1aebf157bb2
kareemkarawan <150752157+kareemkarawan@users.noreply.github.com>
2023-12-23 16:13:03 +0000

    scripts.js

--- Internet Applications CW1/scripts.js
+++ Internet Applications CW1/scripts.js
@@ -2,3 +2,6 @@ function navIcon(x){
     x.classList.toggle("change");
     document.getElementById("myDropdown").classList.toggle("show")
 }
+function nightMode(x){
+        document.getElementById('navim').src = 'images/background.jpg';
+}
